ce14c0e8fb40831d476c53247b15abee4f9bfaeb
Manish <nickoo1576@gmail.com>
2022-05-08 17:14:35 +0530

    adding semester insterface

---
+++ Student_mgmt/src/com/application/semester/semester_details.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.application.semester;
+
+/**
+ * @author Manish
+ *
+ */
+public interface semester_details {
+	
+	
+	//create semester
+	abstract String create_semester();
+	
+	//Read semester
+	abstract String read_semester();
+	
+	//update semester
+	abstract String update_semester();
+	
+	//delete semester
+	abstract String delete_semester();
+
+}
